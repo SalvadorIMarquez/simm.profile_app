050cb6fc94d5bc2514901efe180961d3a89542b1
Salvador Marquez <imt.simm@hotmail.com>
2019-04-30 16:42:37 -0500

    Inicio de nuevo proyecto

---
+++ Profile App/Profile App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profile App
+//
+//  Created by Salvador Marquez on 4/30/19.
+//  Copyright © 2019 Citsa Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
